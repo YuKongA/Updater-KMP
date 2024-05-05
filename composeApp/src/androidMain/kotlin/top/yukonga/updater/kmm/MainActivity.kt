80d4cc1a5a1bb1c3f2cd3f565ec5a808ad0fe092
余空 <70465933+YuKongA@users.noreply.github.com>
2024-05-05 15:41:37 +0800

    android: enableEdgeToEdge

--- composeApp/src/androidMain/kotlin/top/yukonga/updater/kmm/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/top/yukonga/updater/kmm/MainActivity.kt
@@ -4,15 +4,15 @@ import App
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
 
         setContent {
             App()
@@ -23,5 +23,5 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun AppAndroidPreview() {
-        App()
+    App()
 }
