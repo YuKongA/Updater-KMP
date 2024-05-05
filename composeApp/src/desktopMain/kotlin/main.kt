ed497240587335630d8b94e7e53dcc6fdf2285a4
余空 <70465933+YuKongA@users.noreply.github.com>
2024-05-05 15:51:40 +0800

    desktop: Set application size

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,10 +1,17 @@
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 
 fun main() = application {
+    val state = rememberWindowState(
+        size = DpSize(420.dp, 780.dp), position = WindowPosition.Aligned(Alignment.Center)
+    )
     Window(
-        onCloseRequest = ::exitApplication,
-        title = "Updater KMM",
+        onCloseRequest = ::exitApplication, title = "Updater KMM", state = state
     ) {
         App()
     }
