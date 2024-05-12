cf69243a871f68085750899b97b629d8fffdbdde
余空 <70465933+YuKongA@users.noreply.github.com>
2024-05-12 14:28:20 +0800

    android: Make MIUI/HyperOS happy

--- composeApp/src/androidMain/kotlin/top/yukonga/updater/kmm/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/top/yukonga/updater/kmm/MainActivity.kt
@@ -1,6 +1,7 @@
 package top.yukonga.updater.kmm
 
 import App
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,10 +10,10 @@ import androidx.activity.enableEdgeToEdge
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         AndroidAppContext.init(this)
 
         enableEdgeToEdge()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) window.isNavigationBarContrastEnforced = false
 
         setContent {
             App()
