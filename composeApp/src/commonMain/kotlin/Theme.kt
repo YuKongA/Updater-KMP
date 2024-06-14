60a644ebd65cd3a6f978ad71220273f0b377eb96
余空 <70465933+YuKongA@users.noreply.github.com>
2024-06-14 23:09:54 +0800

    app: Fix dark mode

--- composeApp/src/commonMain/kotlin/Theme.kt
+++ composeApp/src/commonMain/kotlin/Theme.kt
@@ -1,13 +1,20 @@
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 
 @Composable
 fun AppTheme(
     content: @Composable () -> Unit
 ) {
+    val darkTheme = isSystemInDarkTheme()
     return MaterialTheme(
-        colorScheme = platformColor() ?: MaterialTheme.colorScheme,
+        colorScheme = platformColor() ?: when {
+            darkTheme -> darkColorScheme()
+            else -> lightColorScheme()
+        },
         shapes = MaterialTheme.shapes,
         typography = MaterialTheme.typography,
         content = content
