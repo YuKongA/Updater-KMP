028dff29ba62ab20fdf22b28151a2976578efff1
YuKongA <70465933+YuKongA@users.noreply.github.com>
2024-10-15 19:19:54 +0800

    build(deps): bump top.yukonga.miuix.kmp:miuix from 0.2.7 to 0.2.8

--- composeApp/src/commonMain/kotlin/ui/AboutDialog.kt
+++ composeApp/src/commonMain/kotlin/ui/AboutDialog.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Update
 import androidx.compose.material.icons.rounded.Update
@@ -36,8 +37,7 @@ import org.jetbrains.compose.resources.stringResource
 import top.yukonga.miuix.kmp.basic.Text
 import top.yukonga.miuix.kmp.extra.SuperDialog
 import top.yukonga.miuix.kmp.theme.MiuixTheme
-import top.yukonga.miuix.kmp.utils.MiuixPopupUtil.Companion.showDialog
-import top.yukonga.miuix.kmp.utils.squircleshape.SquircleShape
+import top.yukonga.miuix.kmp.utils.MiuixPopupUtil.Companion.dismissDialog
 import updater.composeapp.generated.resources.Res
 import updater.composeapp.generated.resources.about
 import updater.composeapp.generated.resources.app_name
@@ -64,92 +64,89 @@ fun AboutDialog() {
         )
     }
 
-    if (!showDialog.value) return
-    showDialog(
-        content = {
-            SuperDialog(
-                show = showDialog,
-                title = stringResource(Res.string.about),
-                onDismissRequest = { showDialog.value = false }
+    SuperDialog(
+        show = showDialog,
+        title = stringResource(Res.string.about),
+        onDismissRequest = {
+            dismissDialog(showDialog)
+        }
+    ) {
+        Row(
+            modifier = Modifier.padding(bottom = 10.dp),
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .size(48.dp)
+                    .clip(RoundedCornerShape(12.dp))
+                    .background(MiuixTheme.colorScheme.primary)
             ) {
-                Row(
-                    modifier = Modifier.padding(bottom = 10.dp),
-                    horizontalArrangement = Arrangement.spacedBy(16.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Box(
-                        contentAlignment = Alignment.Center,
-                        modifier = Modifier
-                            .size(48.dp)
-                            .clip(SquircleShape(13.dp))
-                            .background(MiuixTheme.colorScheme.primary)
-                    ) {
-                        Image(
-                            imageVector = Icons.Outlined.Update,
-                            colorFilter = ColorFilter.tint(Color.White),
-                            contentDescription = null,
-                            modifier = Modifier.size(25.dp),
-                        )
-                    }
-                    Column {
-                        Text(
-                            text = stringResource(Res.string.app_name),
-                            fontSize = 22.sp,
-                            fontWeight = FontWeight.SemiBold
-                        )
-                        Text(
-                            text = VersionInfo.VERSION_NAME + " (" + VersionInfo.VERSION_CODE + ")",
-                        )
-                    }
-                }
-                val uriHandler = LocalUriHandler.current
-                Row(
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(
-                        text = stringResource(Res.string.view_source) + " ",
-                    )
-                    Text(
-                        text = AnnotatedString(
-                            text = "GitHub",
-                            spanStyle = SpanStyle(
-                                textDecoration = TextDecoration.Underline,
-                                color = MiuixTheme.colorScheme.primary
-                            )
-                        ),
-                        modifier = Modifier.clickable(
-                            onClick = {
-                                uriHandler.openUri("https://github.com/YuKongA/Updater-KMP")
-                            }
-                        )
-                    )
-                }
-                Row(
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(
-                        text = stringResource(Res.string.join_group) + " ",
-                    )
-                    Text(
-                        text = AnnotatedString(
-                            text = "Telegram",
-                            spanStyle = SpanStyle(
-                                textDecoration = TextDecoration.Underline,
-                                color = MiuixTheme.colorScheme.primary
-                            )
-                        ),
-                        modifier = Modifier.clickable(
-                            onClick = {
-                                uriHandler.openUri("https://t.me/YuKongA13579")
-                            },
-                        )
-                    )
-                }
+                Image(
+                    imageVector = Icons.Outlined.Update,
+                    colorFilter = ColorFilter.tint(Color.White),
+                    contentDescription = null,
+                    modifier = Modifier.size(25.dp),
+                )
+            }
+            Column {
                 Text(
-                    modifier = Modifier.padding(top = 10.dp),
-                    text = stringResource(Res.string.opensource_info)
+                    text = stringResource(Res.string.app_name),
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.SemiBold
+                )
+                Text(
+                    text = VersionInfo.VERSION_NAME + " (" + VersionInfo.VERSION_CODE + ")",
                 )
             }
         }
-    )
+        val uriHandler = LocalUriHandler.current
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = stringResource(Res.string.view_source) + " ",
+            )
+            Text(
+                text = AnnotatedString(
+                    text = "GitHub",
+                    spanStyle = SpanStyle(
+                        textDecoration = TextDecoration.Underline,
+                        color = MiuixTheme.colorScheme.primary
+                    )
+                ),
+                modifier = Modifier.clickable(
+                    onClick = {
+                        uriHandler.openUri("https://github.com/YuKongA/Updater-KMP")
+                    }
+                )
+            )
+        }
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = stringResource(Res.string.join_group) + " ",
+            )
+            Text(
+                text = AnnotatedString(
+                    text = "Telegram",
+                    spanStyle = SpanStyle(
+                        textDecoration = TextDecoration.Underline,
+                        color = MiuixTheme.colorScheme.primary
+                    )
+                ),
+                modifier = Modifier.clickable(
+                    onClick = {
+                        uriHandler.openUri("https://t.me/YuKongA13579")
+                    },
+                )
+            )
+        }
+        Text(
+            modifier = Modifier.padding(top = 10.dp),
+            text = stringResource(Res.string.opensource_info)
+        )
+    }
 }
--- composeApp/src/commonMain/kotlin/ui/LoginDialog.kt
+++ composeApp/src/commonMain/kotlin/ui/LoginDialog.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
@@ -44,7 +43,6 @@ import top.yukonga.miuix.kmp.basic.TextField
 import top.yukonga.miuix.kmp.extra.SuperDialog
 import top.yukonga.miuix.kmp.theme.MiuixTheme
 import top.yukonga.miuix.kmp.utils.MiuixPopupUtil.Companion.dismissDialog
-import top.yukonga.miuix.kmp.utils.MiuixPopupUtil.Companion.showDialog
 import updater.composeapp.generated.resources.Res
 import updater.composeapp.generated.resources.account
 import updater.composeapp.generated.resources.account_or_password_empty
@@ -90,7 +88,6 @@ fun LoginDialog(
     val messageCrashInfo = stringResource(Res.string.toast_crash_info)
 
     IconButton(
-        modifier = Modifier.size(32.dp),
         onClick = {
             showDialog.value = true
             hapticFeedback.performHapticFeedback(HapticFeedbackType.LongPress)
@@ -103,153 +100,147 @@ fun LoginDialog(
         )
     }
 
-    if (!showDialog.value) return
-
     if (isLogin.value != 1) {
-        showDialog(
-            content = {
-                SuperDialog(
-                    show = showDialog,
-                    title = stringResource(Res.string.login),
-                    onDismissRequest = { showDialog.value = false }
+        SuperDialog(
+            show = showDialog,
+            title = stringResource(Res.string.login),
+            onDismissRequest = {
+                dismissDialog(showDialog)
+            }
+        ) {
+            Column {
+                TextField(
+                    value = account,
+                    onValueChange = { account = it },
+                    label = stringResource(Res.string.account),
+                    modifier = Modifier.fillMaxWidth(),
+                    singleLine = true,
+                )
+                var passwordVisibility by remember { mutableStateOf(false) }
+                TextField(
+                    value = password,
+                    onValueChange = { password = it },
+                    label = stringResource(Res.string.password),
+                    modifier = Modifier.fillMaxWidth().padding(top = 16.dp),
+                    singleLine = true,
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                    visualTransformation = if (passwordVisibility) VisualTransformation.None else PasswordVisualTransformation(),
+                    trailingIcon = {
+                        IconButton(
+                            onClick = { passwordVisibility = !passwordVisibility },
+                            content = {
+                                Icon(
+                                    imageVector = Icons.Rounded.Visibility,
+                                    tint = if (passwordVisibility) MiuixTheme.colorScheme.primary else MiuixTheme.colorScheme.onSecondaryContainer,
+                                    contentDescription = null
+                                )
+                            }
+                        )
+                    }
+                )
+                Row(
+                    modifier = Modifier.padding(vertical = 16.dp),
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Column {
-                        TextField(
-                            value = account,
-                            onValueChange = { account = it },
-                            label = stringResource(Res.string.account),
-                            modifier = Modifier.fillMaxWidth(),
-                            singleLine = true,
+                    Row(
+                        modifier = Modifier.weight(1f),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Checkbox(
+                            modifier = Modifier.padding(end = 10.dp),
+                            checked = global,
+                            onCheckedChange = {
+                                global = it
+                                hapticFeedback.performHapticFeedback(HapticFeedbackType.LongPress)
+                            })
+                        Text(
+                            text = stringResource(Res.string.global)
                         )
-                        var passwordVisibility by remember { mutableStateOf(false) }
-                        TextField(
-                            value = password,
-                            onValueChange = { password = it },
-                            label = stringResource(Res.string.password),
-                            modifier = Modifier.fillMaxWidth().padding(top = 16.dp),
-                            singleLine = true,
-                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-                            visualTransformation = if (passwordVisibility) VisualTransformation.None else PasswordVisualTransformation(),
-                            trailingIcon = {
-                                IconButton(
-                                    onClick = { passwordVisibility = !passwordVisibility },
-                                    content = {
-                                        Icon(
-                                            imageVector = Icons.Rounded.Visibility,
-                                            tint = if (passwordVisibility) MiuixTheme.colorScheme.primary else MiuixTheme.colorScheme.onSecondaryContainer,
-                                            contentDescription = null
-                                        )
-                                    }
-                                )
-                            }
+                    }
+                    Row(
+                        modifier = Modifier.weight(1f),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Checkbox(
+                            modifier = Modifier.padding(end = 10.dp),
+                            checked = savePassword == "1",
+                            onCheckedChange = {
+                                savePassword = if (it) "1" else "0"
+                                hapticFeedback.performHapticFeedback(HapticFeedbackType.LongPress)
+                            })
+                        Text(
+                            text = stringResource(Res.string.save_password)
                         )
-                        Row(
-                            modifier = Modifier.padding(vertical = 16.dp),
-                            verticalAlignment = Alignment.CenterVertically
-                        ) {
-                            Row(
-                                modifier = Modifier.weight(1f),
-                                horizontalArrangement = Arrangement.Center
-                            ) {
-                                Checkbox(
-                                    modifier = Modifier.padding(end = 10.dp),
-                                    checked = global,
-                                    onCheckedChange = {
-                                        global = it
-                                        hapticFeedback.performHapticFeedback(HapticFeedbackType.LongPress)
-                                    })
-                                Text(
-                                    text = stringResource(Res.string.global)
-                                )
-                            }
-                            Row(
-                                modifier = Modifier.weight(1f),
-                                horizontalArrangement = Arrangement.Center
-                            ) {
-                                Checkbox(
-                                    modifier = Modifier.padding(end = 10.dp),
-                                    checked = savePassword == "1",
-                                    onCheckedChange = {
-                                        savePassword = if (it) "1" else "0"
-                                        hapticFeedback.performHapticFeedback(HapticFeedbackType.LongPress)
-                                    })
-                                Text(
-                                    text = stringResource(Res.string.save_password)
-                                )
-                            }
-                        }
-                        Row {
-                            Button(
-                                modifier = Modifier.weight(1f),
-                                text = stringResource(Res.string.login),
-                                submit = true,
-                                onClick = {
-                                    dismissDialog(showDialog)
-                                    showMessage(message = messageLoginIn)
-                                    hapticFeedback.performHapticFeedback(HapticFeedbackType.LongPress)
-                                    coroutineScope.launch {
-                                        val int = login(account, password, global, savePassword, isLogin)
-                                        when (int) {
-                                            0 -> showMessage(message = messageLoginSuccess)
-                                            1 -> showMessage(message = messageEmpty)
-                                            2 -> showMessage(message = messageCrashInfo)
-                                            3 -> showMessage(message = messageError)
-                                            4 -> showMessage(message = messageSecurityError)
-                                        }
-                                    }
-                                }
-                            )
-                            Spacer(Modifier.width(20.dp))
-                            Button(
-                                modifier = Modifier.weight(1f),
-                                text = stringResource(Res.string.cancel),
-                                onClick = {
-                                    dismissDialog(showDialog)
-                                    hapticFeedback.performHapticFeedback(HapticFeedbackType.LongPress)
-                                }
-                            )
-                        }
                     }
                 }
+                Row {
+                    Button(
+                        modifier = Modifier.weight(1f),
+                        text = stringResource(Res.string.login),
+                        submit = true,
+                        onClick = {
+                            dismissDialog(showDialog)
+                            showMessage(message = messageLoginIn)
+                            hapticFeedback.performHapticFeedback(HapticFeedbackType.LongPress)
+                            coroutineScope.launch {
+                                val int = login(account, password, global, savePassword, isLogin)
+                                when (int) {
+                                    0 -> showMessage(message = messageLoginSuccess)
+                                    1 -> showMessage(message = messageEmpty)
+                                    2 -> showMessage(message = messageCrashInfo)
+                                    3 -> showMessage(message = messageError)
+                                    4 -> showMessage(message = messageSecurityError)
+                                }
+                            }
+                        }
+                    )
+                    Spacer(Modifier.width(20.dp))
+                    Button(
+                        modifier = Modifier.weight(1f),
+                        text = stringResource(Res.string.cancel),
+                        onClick = {
+                            dismissDialog(showDialog)
+                            hapticFeedback.performHapticFeedback(HapticFeedbackType.LongPress)
+                        }
+                    )
+                }
             }
-        )
+        }
     }
 
     if (isLogin.value == 1) {
-        showDialog(
-            content = {
-                SuperDialog(
-                    show = showDialog,
-                    title = stringResource(Res.string.logout),
-                    summary = stringResource(Res.string.logout_confirm),
-                    onDismissRequest = { showDialog.value = false }
-                ) {
-                    Row {
-                        Button(
-                            modifier = Modifier.weight(1f),
-                            text = stringResource(Res.string.logout),
-                            submit = true,
-                            onClick = {
-                                coroutineScope.launch {
-                                    val boolean = logout(isLogin)
-                                    if (boolean) showMessage(message = messageLogoutSuccessful)
-                                }
-                                dismissDialog(showDialog)
-                                hapticFeedback.performHapticFeedback(HapticFeedbackType.LongPress)
-                            }
-                        )
-                        Spacer(Modifier.width(20.dp))
-                        Button(
-                            modifier = Modifier.weight(1f),
-                            text = stringResource(Res.string.cancel),
-                            onClick = {
-                                dismissDialog(showDialog)
-                                hapticFeedback.performHapticFeedback(HapticFeedbackType.LongPress)
-                            }
-                        )
-                    }
-                }
+        SuperDialog(
+            show = showDialog,
+            title = stringResource(Res.string.logout),
+            summary = stringResource(Res.string.logout_confirm),
+            onDismissRequest = {
+                dismissDialog(showDialog)
             }
-        )
+        ) {
+            Row {
+                Button(
+                    modifier = Modifier.weight(1f),
+                    text = stringResource(Res.string.logout),
+                    submit = true,
+                    onClick = {
+                        coroutineScope.launch {
+                            val boolean = logout(isLogin)
+                            if (boolean) showMessage(message = messageLogoutSuccessful)
+                        }
+                        dismissDialog(showDialog)
+                        hapticFeedback.performHapticFeedback(HapticFeedbackType.LongPress)
+                    }
+                )
+                Spacer(Modifier.width(20.dp))
+                Button(
+                    modifier = Modifier.weight(1f),
+                    text = stringResource(Res.string.cancel),
+                    onClick = {
+                        dismissDialog(showDialog)
+                        hapticFeedback.performHapticFeedback(HapticFeedbackType.LongPress)
+                    }
+                )
+            }
+        }
     }
 }
