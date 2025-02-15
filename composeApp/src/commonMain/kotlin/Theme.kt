30fe7e943019795372a1be40aa4a7c1db6ffab25
YuKongA <70465933+YuKongA@users.noreply.github.com>
2025-02-15 12:33:25 +0800

    app: Cleanup MiuixTheme

--- composeApp/src/commonMain/kotlin/Theme.kt
+++ composeApp/src/commonMain/kotlin/Theme.kt
@@ -1,13 +1,8 @@
-import androidx.compose.foundation.LocalIndication
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.remember
-import top.yukonga.miuix.kmp.theme.LocalColors
 import top.yukonga.miuix.kmp.theme.MiuixTheme
 import top.yukonga.miuix.kmp.theme.darkColorScheme
 import top.yukonga.miuix.kmp.theme.lightColorScheme
-import top.yukonga.miuix.kmp.utils.MiuixIndication
 
 @Composable
 fun AppTheme(
@@ -16,14 +11,6 @@ fun AppTheme(
     MiuixTheme(
         colors = if (isSystemInDarkTheme()) darkColorScheme() else lightColorScheme()
     ) {
-        val colors = LocalColors.current
-        val miuixIndication = remember(colors.onBackground) {
-            MiuixIndication(backgroundColor = colors.onBackground)
-        }
-        CompositionLocalProvider(
-            LocalIndication provides miuixIndication
-        ) {
-            content()
-        }
+        content()
     }
 }
