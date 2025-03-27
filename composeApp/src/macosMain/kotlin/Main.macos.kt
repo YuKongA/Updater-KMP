2255e9f1ae225ef6647ec6d5638bc14b2d1f6006
YuKongA <70465933+YuKongA@users.noreply.github.com>
2025-03-27 14:25:46 +0800

    app: macos: Close the app properly

--- composeApp/src/macosMain/kotlin/Main.macos.kt
+++ composeApp/src/macosMain/kotlin/Main.macos.kt
@@ -3,9 +3,25 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import platform.AppKit.NSApp
 import platform.AppKit.NSApplication
+import platform.AppKit.NSApplicationDelegateProtocol
+import platform.Foundation.NSNotification
+import platform.darwin.NSObject
+import kotlin.system.exitProcess
+
+class AppDelegate : NSObject(), NSApplicationDelegateProtocol {
+    override fun applicationShouldTerminateAfterLastWindowClosed(sender: NSApplication): Boolean {
+        return true
+    }
+
+    override fun applicationWillTerminate(notification: NSNotification) {
+        exitProcess(0)
+    }
+}
 
 fun main() {
     NSApplication.sharedApplication()
+    val delegate = AppDelegate()
+    NSApp?.setDelegate(delegate)
     Window(
         title = "Updater",
         size = DpSize(420.dp, 820.dp)
