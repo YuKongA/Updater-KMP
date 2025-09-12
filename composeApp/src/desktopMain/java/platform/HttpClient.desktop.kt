1e2b52220ded2abcaa6d0fb778facce2d9037805
YuKongA <70465933+YuKongA@users.noreply.github.com>
2025-09-12 11:13:51 +0800

    Revert "13"
    This reverts commit 6fed6379f955461b604823a5601fb8784dea8a0f.

--- composeApp/src/desktopMain/java/platform/HttpClient.desktop.kt
+++ composeApp/src/desktopMain/java/platform/HttpClient.desktop.kt
@@ -5,14 +5,9 @@ import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.HttpRedirect
 import io.ktor.client.plugins.cookies.AcceptAllCookiesStorage
 import io.ktor.client.plugins.cookies.HttpCookies
-import io.ktor.client.plugins.defaultRequest
-import io.ktor.client.request.header
 
 actual fun httpClientPlatform(): HttpClient {
     return HttpClient(CIO) {
-        defaultRequest {
-            header("Accept-Encoding", "gzip, deflate, br")
-        }
         install(HttpCookies) {
             storage = AcceptAllCookiesStorage()
         }
