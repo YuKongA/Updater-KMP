6fed6379f955461b604823a5601fb8784dea8a0f
YuKongA <70465933+YuKongA@users.noreply.github.com>
2025-09-12 11:11:13 +0800

    13

--- composeApp/src/iosMain/kotlin/platform/HttpClient.ios.kt
+++ composeApp/src/iosMain/kotlin/platform/HttpClient.ios.kt
@@ -5,9 +5,14 @@ import io.ktor.client.engine.darwin.Darwin
 import io.ktor.client.plugins.HttpRedirect
 import io.ktor.client.plugins.cookies.AcceptAllCookiesStorage
 import io.ktor.client.plugins.cookies.HttpCookies
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.request.header
 
 actual fun httpClientPlatform(): HttpClient {
     return HttpClient(Darwin).config {
+        defaultRequest {
+            header("Accept-Encoding", "gzip, deflate, br")
+        }
         install(HttpCookies) {
             storage = AcceptAllCookiesStorage()
         }
